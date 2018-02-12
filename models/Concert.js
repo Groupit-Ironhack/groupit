cfb33933355d6020b40474c2887e2ba96b50549b
Ana Recuero <anarecsa@gmail.com>
2018-02-12 16:24:37 +0100

    Concert model modified

--- models/Concert.js
+++ models/Concert.js
@@ -3,29 +3,15 @@ const Schema = mongoose.Schema;
 
 const concertSchema = new Schema(
   {
-    artist: String,
-    description: String,
-    location: {
-      name : String,
-      address: {
-        number: Number,
-        street: String,
-        city: String,
-        state: String,
-        country: String,
-        zip: Number
-      },
-      coordinates: {
-        lat: Number,
-        lng: Number
-      }
-    },
+    concertId: String,
+    locationId: String,
+    date: Date,
     attendants: [
       { 
         type : Schema.Types.ObjectId, 
         ref: 'User' 
       }
-    ]
+    ],
   },
   {
     timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
