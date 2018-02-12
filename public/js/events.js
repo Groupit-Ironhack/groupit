f7a7a9ae54a09bf0452c1304690889dc51896dba
Ana Recuero <anarecsa@gmail.com>
2018-02-12 23:39:35 +0100

    concert search data list

--- public/js/events.js
+++ public/js/events.js
@@ -1,8 +1,10 @@
 const printConcert = (concert) => {
-  let div = $("<div>")
-  let h4 = $("<h4>").text(concert.title).attr("data-id", concert.id).addClass("concert");
-  let loc = $("<p>").text(concert.venue_name);
-  div.append(h4).append(loc);
+  console.log(concert);
+  let div = $("<div>").addClass("concert-card")
+  let h3 = $("<h3>").text(concert.title).attr("data-id", concert.id).addClass("concert");
+  let loc = $("<p>").text(`${concert.venue_name} - ${concert.city_name}`);
+  let date = $("<p>").text(concert.start_time);
+  div.append(h3).append(loc).append(date);
   $("#search-results").append(div);
 };
 
@@ -12,8 +14,9 @@ function show_events() {
     q: "music",
     where: document.getElementById("where").value,
     page_size: 6,
+    date: "Future",
     sort_order: "popularity",
-    keywords: document.getElementById("performer").value
+    keywords: "concert "+ document.getElementById("performer").value
   };
 
   EVDB.API.call("/events/search", oArgs, function(oData) {
