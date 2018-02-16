ba0dd22e3764d0923f7f3234733ddcc5132b5e42
Ana Recuero <anarecsa@gmail.com>
2018-02-16 14:09:09 +0100

    fix del fixxxxx

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,6 +1,6 @@
 $("#create").on("click", (e) =>{
 
-  if($("#pac-input").val() == "" || $("#description").val() == "" || $("#time")){
+  if($("#pac-input").val() == "" || $("#description").val() == "" || $("#time").val() == ""){
     e.preventDefault();
     $(".alert-danger").addClass("d-block")
     $(".alert-danger").removeClass("d-none")
