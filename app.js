746a2e9cf7cf0b1b025188435e639d3f547e5a2e
Ana Recuero <anarecsa@gmail.com>
2018-02-16 11:29:27 +0100

    styles

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -33,8 +32,6 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set('layout','layouts/main-layout');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
